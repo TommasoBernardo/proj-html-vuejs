c65c18ede35af36a7e019880aad12d5ba890ab38
TommasoBernardo <tommasob1210@gmail.com>
2023-01-18 16:12:18 +0100

    setup components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script >
+import ComponentsHeader from './components/ComponentsHeader.vue';
+import ComponentsFooter from './components/ComponentsFooter.vue';
+import MainAppForum from './components/main/MainAppForum.vue';
+import MainForum from './components/main/MainForum.vue';
+import MainJoin from './components/main/MainJoin.vue';
+import MainLastNews from './components/main/MainLastNews.vue';
 
+export default{
+  components: {
+    ComponentsHeader,
+    ComponentsFooter,
+    MainAppForum,
+    MainForum,
+    MainJoin,
+    MainLastNews
+  }
+}
 </script>
 
 <template>
@@ -7,7 +23,7 @@
 </template>
 
 <style lang="scss">
-@use './styles/general.scss' as *; 
-@use './styles/partials/variables' as *; 
+@use './Style/general.scss' as *; 
+@use './Style/partials/variables' as *; 
 @use 'bootstrap/scss/bootstrap.scss' as *;
 </style>
---
+++ src/components/ComponentsFooter.vue
@@ -0,0 +1,17 @@
+
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+
+<style lang="">
+    
+</style>
