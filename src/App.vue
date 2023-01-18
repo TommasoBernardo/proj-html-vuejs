dba73800d5642434908ce4e30bbf35a9e0543cc4
TommasoBernardo <tommasob1210@gmail.com>
2023-01-18 16:05:33 +0100

    create file scss

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use './styles/general.scss' as *; 
+@use './styles/partials/variables' as *; 
+@use 'bootstrap/scss/bootstrap.scss' as *;
 </style>
