d3a53a322bd125baa9138bb5f3d239911ceb1cc7
TommasoBernardo <tommasob1210@gmail.com>
2023-01-19 19:17:29 +0100

    updtate and add cards

--- src/components/main/MainLastNews.vue
+++ src/components/main/MainLastNews.vue
@@ -23,8 +23,8 @@ export default {
         </div>
 
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-4">
+            <div class="row justify-content-center ">
+                <div class="col-4 ">
                     <img class="my-img" src="../../assets/img/post_feat_img_25-320x202.jpg" alt="phone">
                     <p>
                         <strong>Morvi vitae dui euismod vulputate sollicitutid</strong><br>
@@ -38,7 +38,7 @@ export default {
                 <div class="col-4">
                     <img class="my-img" src="../../assets/img/post_feat_img_24-320x202.jpg" alt="phone">
                     <p>
-                        <strong>Morvi vitae dui euismod vulputate sollicitutid</strong><br>
+                        <strong>Vivamus pellenteque, felis quis varius</strong><br>
                         <span class="my-text">october 11th,2015</span> | <span class="comments">comments off</span> 
                     </p>
                     <p class="my-text">
@@ -49,7 +49,60 @@ export default {
                 <div class="col-4">
                     <img class="my-img" src="../../assets/img/post_feat_img_23-320x202.jpg" alt="phone">
                     <p>
-                        <strong>Morvi vitae dui euismod vulputate sollicitutid</strong><br>
+                        <strong>Donec ornare pretium eget scelisque justo</strong><br>
+                        <span class="my-text">october 11th,2015</span> | <span class="comments">comments 0ff</span>
+                    </p>
+                    <p class="my-text">
+                        Donec finibus amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 my-watch mb-5">
+                    <div class="my-position">
+                        <p class="text-white my-feature">
+                        FEATURED ARTICLE
+                    </p>
+                    <h3 class="text-white my-title">
+                        <strong>Cras malesuada et orci eget pharetra</strong> 
+                    </h3>
+                    <p class="text-white pb-3">
+                        In quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum sadips ets ipsum dolores ficilis uns leo lectus.
+                    </p>
+                    <button class="text-white">
+                        <strong>READ MORE ></strong>
+                    </button>
+                </div>
+            </div>
+
+            <div class="row justify-content-center ">
+                <div class="col-4 ">
+                    <img class="my-img" src="../../assets/img/post_feat_img_22-320x202.jpg" alt="phone">
+                    <p>
+                        <strong>Fusce sollicitundin nunc sed placerat varius</strong><br>
+                        <span class="my-text">october 11th,2015</span> | <span>two comments</span>
+                    </p>
+                    <p class="my-text">
+                        Donec finibus amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+                    </p>
+                </div>
+
+                <div class="col-4">
+                    <img class="my-img" src="../../assets/img/post_feat_img_21-320x202.jpg" alt="phone">
+                    <p>
+                        <strong>Donec facilis sodales leo sit amet laoret</strong><br>
+                        <span class="my-text">october 11th,2015</span> | <span class="comments">comments off</span> 
+                    </p>
+                    <p class="my-text">
+                        Donec finibus amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+                    </p>
+                </div>
+
+                <div class="col-4">
+                    <img class="my-img" src="../../assets/img/post_feat_img_20-320x202.jpg" alt="phone">
+                    <p>
+                        <strong>Proin en purus sed  aru aliquet curabir vens</strong><br>
                         <span class="my-text">october 11th,2015</span> | <span class="comments">comments 0ff</span>
                     </p>
                     <p class="my-text">
@@ -58,6 +111,9 @@ export default {
                 </div>
             </div>
         </div>
+    </div>
+
+
     </main>
 </template>
 
@@ -90,6 +146,31 @@ export default {
 
         .comments{
             color: $color-text;
+        }
+
+        .my-watch{
+            background-image: url(../../assets/img/featured_article_1_bg.jpg);
+            background-size: cover;
+            width: 100%;
+            height: 650px;
+        }
+        button{
+            background-color: $color-black;
+            border: none;
+            width: 150px;
+            height: 50px;
+        }
+
+        .my-position{
+            margin-top: 18rem;
+            margin-left: 6%;
+        }
+
+        .my-feature{
+            font-size: .8rem;
+        }
+        .my-title{
+            font-size: 2.5rem;
         }
     }
 </style>
