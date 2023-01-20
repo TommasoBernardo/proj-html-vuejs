518cb16aa40c7583a8bfd050ebc4f5e2030e64a3
TommasoBernardo <tommasob1210@gmail.com>
2023-01-20 17:59:27 +0100

    background

--- src/components/main/MainSubscribe.vue
+++ src/components/main/MainSubscribe.vue
@@ -1,13 +1,29 @@
 
 <script>
 export default {
-    
+    name: 'MainSubscribe',
 }
 </script>
 
 <template>
+    <main>
+        <div class="bk-ground">
+            <h3>
+                Hey
+            </h3>
+        </div>
+    </main>
 </template>
 
 <style lang="scss">
-
+@use '../../Style/general.scss' as *; 
+main{
+    font-family: 'Fira Sans', sans-serif;
+    .bk-ground{
+        background-image: url(../../assets/img/join_now_cta_bg.jpg);
+        background-size: contain;
+        height: 650px;
+        width: 100%;
+    }
+}
 </style>
